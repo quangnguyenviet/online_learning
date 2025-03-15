75522e3b7db5359f72a848a8607cb24aec763eb3
quang nguyen <quangjr07@gmail.com>
2025-03-15 11:02:44 +0700

    first commit

---
+++ source_code/back-end/online_learning/src/test/java/com/vitube/online_learning/OnlineLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitube.online_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
